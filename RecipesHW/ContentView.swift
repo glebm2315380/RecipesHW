ab7dcb3901f64512aaaff868d2fde97fcbb63539
Zahar Litvinchuk <zahar@raynelz.ru>
2025-09-28 14:32:36 +0300

    Initial Commit

---
+++ RecipesHW/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RecipesHW
+//
+//  Created by Захар Литвинчук on 28.09.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RecipesHW/RecipesHWApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesHWApp.swift
+//  RecipesHW
+//
+//  Created by Захар Литвинчук on 28.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct RecipesHWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
